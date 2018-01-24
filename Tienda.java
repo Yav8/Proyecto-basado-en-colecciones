1eaeeb8eb453d4ceff02dd723622044ce2e3235a
Yav <yavieron@hotmail.es>
2018-01-25 00:51:16 +0100

    Creado método que muestra el listado de productos ordenados de menor a mayor por número de unidades

--- Tienda.java
+++ Tienda.java
@@ -47,17 +47,59 @@ public class Tienda {
         return productosConMayorPrecio;
     }
 
-    public void mostrarlistaDeProductosOrdenadosPorPrecio() {
-        ArrayList<Producto> listaDeProductosOrdenadosPorPrecio = new ArrayList<Producto>(); 
+    public void mostrarlistaDeProductosOrdenadosDeMayorAMenorPorPrecio() {
+        ArrayList<Producto> listaDeProductosOrdenadosDeMayorAMenorPorPrecio = new ArrayList<Producto>(); 
         ArrayList<Producto> copiaDeListaDeProductos = new ArrayList<Producto>();
         
         if(listaDeProductos.size() > 0) {
             copiaDeListaDeProductos.addAll(listaDeProductos);
             while(copiaDeListaDeProductos.size() != 0) {
-                listaDeProductosOrdenadosPorPrecio.addAll(localizaElProductoOrdenadoDeMayorAMenorPorPrecio(copiaDeListaDeProductos));
+                listaDeProductosOrdenadosDeMayorAMenorPorPrecio.addAll(localizaElProductoOrdenadoDeMayorAMenorPorPrecio(copiaDeListaDeProductos));
             }
             
-            for(Producto producto : listaDeProductosOrdenadosPorPrecio) {
+            for(Producto producto : listaDeProductosOrdenadosDeMayorAMenorPorPrecio) {
+                System.out.println(producto.todasLasCaracteristicasDelProducto());
+            }
+        }
+    }
+    
+    public ArrayList<Producto> localizaElProductoOrdenadoDeMenorAMayorPorNumeroDeUnidades(ArrayList<Producto> coleccion) {
+        ArrayList<Producto> productosConMenorNumeroDeUnidades = new ArrayList<Producto>();
+        int numeroDeUnidadesMinimo = 0;
+        int contador = 0;
+        
+        if(coleccion.size() > 0) {
+            numeroDeUnidadesMinimo = coleccion.get(0).obtenerNumeroDeUnidades();
+            for(Producto producto : coleccion) {
+                if(producto.obtenerNumeroDeUnidades() < numeroDeUnidadesMinimo) {
+                    numeroDeUnidadesMinimo = producto.obtenerNumeroDeUnidades();
+                }
+            }
+            
+            while(contador < coleccion.size()) {
+                if(coleccion.get(contador).obtenerNumeroDeUnidades() == numeroDeUnidadesMinimo) {
+                    productosConMenorNumeroDeUnidades.add(coleccion.get(contador));
+                    coleccion.remove(coleccion.get(contador));
+                    contador -= 1;
+                }
+                contador += 1;
+            }
+        }
+        
+        return productosConMenorNumeroDeUnidades;
+    }
+    
+    public void mostrarListaDeProductosConMenorNumeroDeUnidades() {
+        ArrayList<Producto> listaDeProductosOrdenadosDeMenorAMayorPorNumeroDeUnidades = new ArrayList<Producto>(); 
+        ArrayList<Producto> copiaDeListaDeProductos = new ArrayList<Producto>();
+        
+        if(listaDeProductos.size() > 0) {
+            copiaDeListaDeProductos.addAll(listaDeProductos);
+            while(copiaDeListaDeProductos.size() != 0) {
+                listaDeProductosOrdenadosDeMenorAMayorPorNumeroDeUnidades.addAll(localizaElProductoOrdenadoDeMenorAMayorPorNumeroDeUnidades(copiaDeListaDeProductos));
+            }
+            
+            for(Producto producto : listaDeProductosOrdenadosDeMenorAMayorPorNumeroDeUnidades) {
                 System.out.println(producto.todasLasCaracteristicasDelProducto());
             }
         }
