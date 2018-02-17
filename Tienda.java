ed51cdbb2ad12dd0e3ad8a3ec02caf02a20086e3
Yav <yavieron@hotmail.es>
2018-02-17 23:26:00 +0100

    Añadido método que muestra los productos divididos por su número de unidades y modificado el contenido del archivo externo

--- Tienda.java
+++ Tienda.java
@@ -3,11 +3,12 @@ import java.util.Iterator;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 
 public class Tienda {
     private ArrayList<Producto> listaDeProductos;
     private int numeroIdentificativo;
-    
+
     /**
      * Constructor de la clase Tienda. Si el usuario lo desea,
      * puede obtener directamente una lista de productos si introduce
@@ -29,7 +30,7 @@ public class Tienda {
             e.printStackTrace();
         }
     }
-    
+
     /**
      * Añade productos a la colección con valores reales.
      */
@@ -39,7 +40,7 @@ public class Tienda {
             numeroIdentificativo += 1;
         }
     }
-    
+
     /**
      * Muestra las características de todos los productos de la tienda.
      */
@@ -48,7 +49,7 @@ public class Tienda {
             System.out.println(producto.todasLasCaracteristicasDelProducto());
         }
     }
-    
+
     /**
      * Localiza y devuelve aquellos productos con el mayor precio y
      * los elimina de la colección introducida por el usuario.
@@ -57,14 +58,14 @@ public class Tienda {
         ArrayList<Producto> productosConMayorPrecio = new ArrayList<Producto>();
         double precioMaximo = 0;
         int contador = 0;
-        
+
         if(coleccion.size() > 0) {
             for(Producto producto : coleccion) {
                 if(producto.obtenerPrecio() > precioMaximo) {
                     precioMaximo = producto.obtenerPrecio();
                 }
             }
-            
+
             while(contador < coleccion.size()) {
                 if(coleccion.get(contador).obtenerPrecio() == precioMaximo) {
                     productosConMayorPrecio.add(coleccion.get(contador));
@@ -74,7 +75,7 @@ public class Tienda {
                 contador += 1;
             }
         }
-        
+
         return productosConMayorPrecio;
     }
 
@@ -85,19 +86,19 @@ public class Tienda {
     public void mostrarlistaDeProductosOrdenadosDeMayorAMenorPorPrecio() {
         ArrayList<Producto> listaDeProductosOrdenadosDeMayorAMenorPorPrecio = new ArrayList<Producto>(); 
         ArrayList<Producto> copiaDeListaDeProductos = new ArrayList<Producto>();
-        
+
         if(listaDeProductos.size() > 0) {
             copiaDeListaDeProductos.addAll(listaDeProductos);
             while(copiaDeListaDeProductos.size() != 0) {
                 listaDeProductosOrdenadosDeMayorAMenorPorPrecio.addAll(localizaLosProductosConMayorPrecio(copiaDeListaDeProductos));
             }
-            
+
             for(Producto producto : listaDeProductosOrdenadosDeMayorAMenorPorPrecio) {
                 System.out.println(producto.todasLasCaracteristicasDelProducto());
             }
         }
     }
-    
+
     /**
      * Localiza y devuelve los productos con menor número de unidades y
      * los elimina de la colección introducida por el usuario.
@@ -106,7 +107,7 @@ public class Tienda {
         ArrayList<Producto> productosConMenorNumeroDeUnidades = new ArrayList<Producto>();
         int numeroDeUnidadesMinimo = 0;
         int contador = 0;
-        
+
         if(coleccion.size() > 0) {
             numeroDeUnidadesMinimo = coleccion.get(0).obtenerNumeroDeUnidades();
             for(Producto producto : coleccion) {
@@ -114,7 +115,7 @@ public class Tienda {
                     numeroDeUnidadesMinimo = producto.obtenerNumeroDeUnidades();
                 }
             }
-            
+
             while(contador < coleccion.size()) {
                 if(coleccion.get(contador).obtenerNumeroDeUnidades() == numeroDeUnidadesMinimo) {
                     productosConMenorNumeroDeUnidades.add(coleccion.get(contador));
@@ -124,10 +125,10 @@ public class Tienda {
                 contador += 1;
             }
         }
-        
+
         return productosConMenorNumeroDeUnidades;
     }
-    
+
     /**
      * Ordena de menor a mayor el número de unidades de los productos 
      * de la tienda y los muestra por pantalla.
@@ -135,19 +136,19 @@ public class Tienda {
     public void mostrarListaDeProductosDeMenorAMayorNumeroDeUnidades() {
         ArrayList<Producto> listaDeProductosOrdenadosDeMenorAMayorPorNumeroDeUnidades = new ArrayList<Producto>(); 
         ArrayList<Producto> copiaDeListaDeProductos = new ArrayList<Producto>();
-        
+
         if(listaDeProductos.size() > 0) {
             copiaDeListaDeProductos.addAll(listaDeProductos);
             while(copiaDeListaDeProductos.size() != 0) {
                 listaDeProductosOrdenadosDeMenorAMayorPorNumeroDeUnidades.addAll(localizaLosProductosConMenorNumeroDeUnidades(copiaDeListaDeProductos));
             }
-            
+
             for(Producto producto : listaDeProductosOrdenadosDeMenorAMayorPorNumeroDeUnidades) {
                 System.out.println(producto.todasLasCaracteristicasDelProducto());
             }
         }
     }
-    
+
     /**
      * Modifica el nombre, precio y número de unidades 
      * a través del número identificativo indicado por el usuario.
@@ -157,18 +158,39 @@ public class Tienda {
         listaDeProductos.get(numeroIdentificativo - 1).fijarPrecio(precio);
         listaDeProductos.get(numeroIdentificativo - 1).fijarNumeroDeUnidades(numeroDeUnidades);
     }
-    
+
     /**
      * Elimina los objetos que tengan menor número de unidades 
      * que el número introducido por el usuario usando un iterador.
      */
     public void eliminarProductosConMenorNumeroDeUnidadesUtilizandoUnIterador(int numeroDeUnidadesMinimas) {
         Iterator<Producto> it = listaDeProductos.iterator();
-        
+
         while(it.hasNext()) {
             if(it.next().obtenerNumeroDeUnidades() < numeroDeUnidadesMinimas) {
                 it.remove();
             }
+        }
+    }
+
+    /**
+     * Muestra los productos divididos por su número de unidades.
+     */
+    public void mostrarProductosDivididosPorNumeroDeUnidades() {
+        HashSet<Integer> conjuntoDeNumerosDeUnidades = new HashSet<Integer>();
+
+        for(Producto producto : listaDeProductos) {
+            conjuntoDeNumerosDeUnidades.add(producto.obtenerNumeroDeUnidades());
+        }
+
+        for(int numeroDeUnidades : conjuntoDeNumerosDeUnidades) {
+            System.out.println(numeroDeUnidades + ":");            
+            for(Producto producto : listaDeProductos) {
+                if(producto.obtenerNumeroDeUnidades() == numeroDeUnidades) {
+                    System.out.println(producto.todasLasCaracteristicasDelProducto());
+                }
+            }
+            System.out.println();
         }
     }
 }
